3adbca81047dfed31d74d9e1a6f50d27b9e29561
yexingchen <yexingchenyx@163.com>
2022-02-26 11:31:51 +0800

    dllexport

---
+++ 007 dllexport/math.h
@@ -0,0 +1,25 @@
+#ifndef __MATH_H__
+#define __MATH_H__
+
+#ifdef _WIN32
+    #ifdef DLL_EXPORT
+        #define DLL_API __declspec(dllexport)
+    #else
+        #define DLL_API __declspec(dllimport)
+    #endif
+#else
+    #define DLL_API
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+DLL_API int add(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
